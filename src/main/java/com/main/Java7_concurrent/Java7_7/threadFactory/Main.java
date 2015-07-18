bdf8e177da7479b575328524052b52cf98858177
SongKun <satansk@yeah.net>
2015-07-18 20:07:05 +0800

    My ThreadFactory

---
+++ src/main/java/com/main/Java7_concurrent/Java7_7/threadFactory/Main.java
@@ -0,0 +1,21 @@
+package com.main.Java7_concurrent.Java7_7.threadFactory;
+
+/**
+ * Author:  satansk
+ * Date:    20:00 at 2015/7/18
+ * Email:   satansk@hotmail.com
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        MyThreadFactory factory = new MyThreadFactory("MyThreadFactory");
+        Task task = new Task();
+        Thread thread = factory.newThread(task);
+
+        thread.start();
+        thread.join();
+
+        System.out.printf("Main: Thread information:\n");
+        System.out.printf("%s\n", thread);
+        System.out.printf("Main: End.\n");
+    }
+}
