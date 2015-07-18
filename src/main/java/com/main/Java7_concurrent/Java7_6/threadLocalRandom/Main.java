15f386d10f27dceda6a1f9e7f373437ce2a9e81f
SongKun <satansk@yeah.net>
2015-07-18 16:18:30 +0800

    ThreadLocalRandom

---
+++ src/main/java/com/main/Java7_concurrent/Java7_6/threadLocalRandom/Main.java
@@ -0,0 +1,18 @@
+package com.main.Java7_concurrent.Java7_6.threadLocalRandom;
+
+/**
+ * Author:  satansk
+ * Date:    16:03 at 2015/7/18
+ * Email:   satansk@hotmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[3];
+
+        for (int i = 0; i < 3; i++) {
+            TaskLocalRandom task = new TaskLocalRandom();
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
+    }
+}
